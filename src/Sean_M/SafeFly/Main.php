a3c9da97ac082e74870ad7a227aeb5aa732cc31b
Sean <sean@hydreon.com>
2015-11-09 18:54:38 -0500

    Update Main.php

--- src/Sean_M/SafeFly/Main.php
+++ src/Sean_M/SafeFly/Main.php
@@ -1,1 +1,64 @@
+<?php
+namespace Sean_M\SafeFly;
 
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+
+class Main extends PluginBase implements Listener {
+
+    public $players = array();
+
+     public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "SafeFly by Sean_M enabled!");
+     }
+
+     public function onDisable() {
+        $this->getLogger()->info(TextFormat::RED . "SafeFly by Sean_M disabled!");
+     }
+   
+     public function onEntityDamage(EntityDamageEvent $event) {
+        $cause = $event->getCause();
+        if(instace of EntityDamageByEntityEvent && $cause instanceof Player && $this->isPlayer($cause)){
+            $event->setCancelled(true);
+        }
+     }
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args) {
+        if(strtolower($cmd->getName()) == "fly") {
+            if($sender instanceof Player) {
+                if($this->isPlayer($sender)) {
+                    $this->removePlayer($sender);
+                    $sender->sendMessage(TextFormat::RED . "You have disabled fly mode!");
+                    return true;
+                }
+                else{
+                    $this->addPlayer($sender);
+                    $sender->setAllowFlight(true);
+                    $sender->sendMessage(TextFormat::GREEN . "You have enabled fly mode!");
+                    return true;
+                }
+            }
+            else{
+                $sender->sendMessage(TextFormat::RED . "Please use this command in-game.");
+                return true;
+            }
+        }
+    }
+    public function addPlayer(Player $player) {
+        $this->players[$player->getName()] = $player->getName();
+    }
+    public function isPlayer(Player $player) {
+        return in_array($player->getName(), $this->players);
+    }
+    public function removePlayer(Player $player) {
+        unset($this->players[$player->getName()]);
+    }
+}
